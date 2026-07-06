2bda470f6091e1a3cff0a92c1c1373fcfeacdbb0
Rajesh G S <dr06180310@gmail.com>
2026-07-06 12:03:23 +0530

    More Changes

--- auth.py
+++ auth.py
@@ -8,6 +8,7 @@ import os
 load_dotenv()
 
 # Security settings
+DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
